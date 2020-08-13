a178ce6ee0c116d01e10a6bf1f08b66f84dfb76a
ever user <everuser@MacBook-Air-ever.local>
2020-08-13 16:39:36 +0300

    lesson 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const el = (
-  <div>
-    <h1>Todo List</h1>
-    <input placeholder="search"/>
+const AppHeader = () => {
+  return <h1>Todo List</h1>;
+};
+const SearchInput = () => {
+  return <input placeholder="search" />;
+};
+const ListToDo = () => {
+  return (
     <ul>
       <li>Item 1</li>
       <li>Item 2</li>
     </ul>
+  );
+};
+const App = () => {
+  return(
+  <div>
+    <AppHeader />
+    <SearchInput />
+    <ListToDo />
   </div>
-);
+  );
+};
 
-ReactDOM.render(el, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
